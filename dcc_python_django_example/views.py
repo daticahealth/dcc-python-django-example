6fda022d7d1ae2f90164554f826c703fa3f4b7ef
Chris McKenna <chris.mckenna@datica.com>
2021-08-25 18:16:44 -0400

    initial commit

---
+++ dcc_python_django_example/views.py
@@ -0,0 +1,18 @@
+import inspect, os
+from datetime import datetime
+from django.http import HttpResponse
+
+def health_check(request):
+  return HttpResponse('healthy')
+
+def access_log(request):
+  current_loc = os.path.dirname(os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename))
+  access_log_loc = os.getenv('ACCESS_LOG_LOC', f'{current_loc}/../tmp')
+  access_log = f'{access_log_loc}/access_log.txt'
+
+  with open(access_log, 'a') as f:
+    entry = datetime.now()
+    f.write(f'{entry}\n')
+
+  with open(access_log, 'r') as f:
+    return HttpResponse(f.read())
