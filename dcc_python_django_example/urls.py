64cd248cc6984c89692a5a9dcc4e76204b2c5e9a
Chris McKenna <chris.mckenna@datica.com>
2021-08-27 08:51:20 -0400

    fix healthcheck url

--- dcc_python_django_example/urls.py
+++ dcc_python_django_example/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('access/', views.access_log, name='access'),
-    path('_healthcheck', views.health_check, name='healthcheck'),
+    path('_healthcheck/', views.health_check, name='healthcheck'),
 ]
